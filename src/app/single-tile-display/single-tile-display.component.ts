5b24c3408c949b7efef2beeb910086890b9dc143
JimMarshall35 <jimmarshall35@gmail.com>
2020-11-03 20:05:03 +0000

    multiple tiles to edit at once

--- src/app/single-tile-display/single-tile-display.component.ts
+++ src/app/single-tile-display/single-tile-display.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit,AfterViewInit, Input, Output, EventEmitter, SimpleCha
 import { SingleSprite, Palette } from '../processrawchararray.service';
 import { Event } from '@angular/router';
 
+interface wrappedSingleSprite{
+  index : number;
+  sprite : SingleSprite;
+}
 @Component({
   selector: 'app-single-tile-display',
   templateUrl: './single-tile-display.component.html',
@@ -14,17 +18,24 @@ export class SingleTileDisplayComponent implements AfterViewInit {
   @Output() PixelChangedEvent = new EventEmitter<SingleSprite>();
   private _Canvas: HTMLCanvasElement;
   private _Canvases: HTMLCanvasElement[];
-  private _Ctx;
+  private _Ctx: CanvasRenderingContext2D;
   private _scaledtilesize = 250;
   private _pixelsize;
   private _selectiongriddivs;
+  private _currentcanvasindex = 0;
+  private _tiles: wrappedSingleSprite[];
 
   constructor() { }
   ngOnChanges(changes: SimpleChanges): void {
     if ("_SelectedTile" in changes) {
       if(this._SelectedTile != undefined){
         this.SelectedIndex = this.formatHex(this._SelectedTile.Index);
-        this.drawTile();
+        let ctx = this._Canvases[this._currentcanvasindex].getContext("2d");
+        this.drawTile(ctx, this._SelectedTile);
+        //let newobj = new SingleSprite(null,null);
+        //Object.assign(newobj, this._SelectedTile);
+        this.pushToTiles(this._SelectedTile,this._currentcanvasindex);
+        //this._tiles.push({index : this._currentcanvasindex, sprite : this._SelectedTile})
       }
       
     }
@@ -48,6 +59,7 @@ export class SingleTileDisplayComponent implements AfterViewInit {
     this._selectiongriddivs = document.getElementsByClassName("gridcell");
     this.SelectedIndex = this.formatHex(0);
     this._pixelsize = this._scaledtilesize / 8;
+    this._tiles = [];
     //this.drawTile();
   }
   initCanvases(){
@@ -62,15 +74,15 @@ export class SingleTileDisplayComponent implements AfterViewInit {
       canvas.height = this._scaledtilesize;
     }
   }
-  drawTile(): void {
-    this._Ctx.clearRect(0, 0, this._scaledtilesize, this._scaledtilesize);
+  drawTile(ctx, sprite: SingleSprite): void {
+    ctx.clearRect(0, 0, this._scaledtilesize, this._scaledtilesize);
     let cursor = { x: 0, y: 0 };
     
-    for (let i = 0; i < this._SelectedTile.PaletteIndices.length; i++) {
-      let index = this._SelectedTile.PaletteIndices[i];
+    for (let i = 0; i < sprite.PaletteIndices.length; i++) {
+      let index = sprite.PaletteIndices[i];
       let colour = Palette[index];
-      this._Ctx.fillStyle = colour.getCSSString();
-      this._Ctx.fillRect(cursor.x, cursor.y, this._pixelsize, this._pixelsize);
+      ctx.fillStyle = colour.getCSSString();
+      ctx.fillRect(cursor.x, cursor.y, this._pixelsize, this._pixelsize);
       cursor.x += this._pixelsize;
       if (cursor.x > 7 * this._pixelsize) {
         cursor.x = 0;
@@ -79,24 +91,57 @@ export class SingleTileDisplayComponent implements AfterViewInit {
     }
   }
   OnClick(e, sender): void {
-    let xpos = e.layerX;
-    let ypos = e.layerY;
-    let row = Math.floor(ypos / this._pixelsize);
-    let col = Math.floor(xpos / this._pixelsize);
-    let index = (row * 8) + col;
-    this._SelectedTile.PaletteIndices[index]++;
-    if (this._SelectedTile.PaletteIndices[index] > 3) {
-      this._SelectedTile.PaletteIndices[index] = 0;
+    let c_index = this.getIndexOfCanvas(sender);
+    let ctx = <HTMLCanvasElement>sender.getContext("2d");
+    let sprite = this.getSpriteByIndex(c_index);
+    if(sprite != null){
+      let xpos = e.layerX;
+      let ypos = e.layerY;
+      let row = Math.floor(ypos / this._pixelsize);
+      let col = Math.floor(xpos / this._pixelsize);
+      let index = (row * 8) + col;
+      sprite.PaletteIndices[index]++;
+      if (sprite.PaletteIndices[index]>3) {
+        sprite.PaletteIndices[index] = 0;
+      }
+      sprite.processPaletteIndicesToBytes();
+
+      this.drawTile(ctx, sprite);
+      sprite.processPaletteIndicesToImgData();
+      this.PixelChangedEvent.emit(sprite);
     }
-    this._SelectedTile.processPaletteIndicesToBytes();
-    this.drawTile();
-    this._SelectedTile.processPaletteIndicesToImgData();
-    this.PixelChangedEvent.emit(this._SelectedTile);
+    
   }
   OnCellClick(num: number, element){
     for (var i = 0; i < this._selectiongriddivs.length; ++i) {
         this._selectiongriddivs[i].style.backgroundColor = "white";
     }
     element.style.backgroundColor = "red";
+    this._currentcanvasindex = num;
+  }
+  getSpriteByIndex(index:number){
+    for(let i=0; i<this._tiles.length; i++){
+      if(this._tiles[i].index == index){
+        return this._tiles[i].sprite;
+      }
+    }
+    return null;
+  }
+  getIndexOfCanvas(c: HTMLCanvasElement){
+      for (var i = 0; i < this._Canvases.length; i++) {
+        if(c == this._Canvases[i]){
+          return i;
+        }
+      }
+      return null;
+  }
+  pushToTiles(ssprite: SingleSprite, canvasindex: number){
+    for (var i = 0; i < this._tiles.length; i++) {
+      if(this._tiles[i].index == canvasindex){
+        this._tiles.splice(i,1,{index:canvasindex, sprite:ssprite})
+        return;
+      }
+    }
+    this._tiles.push({index:canvasindex, sprite:ssprite});
   }
 }
